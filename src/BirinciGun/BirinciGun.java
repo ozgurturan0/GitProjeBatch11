9ea91085c5fdae1b5042d2a1186c7240da78391c
ozgur <ozgurturan0@gmail.com>
2020-11-17 00:01:31 +0300

    ikinci commit

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -3,7 +3,9 @@ package BirinciGun;
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("11:05 - Birinci Gun");
+		System.out.println("ikinci commit");
 
 	}
 
