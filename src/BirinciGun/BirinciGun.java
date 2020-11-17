c5b2f1ec812b102de0d05669c045c8a8af473064
ozgur <ozgurturan0@gmail.com>
2020-11-17 23:34:06 +0300

    commit

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -11,7 +11,7 @@ public class BirinciGun {
 		System.out.println("10:35 - Ikinci Gun");
 		System.out.println("11:05 - local");
 		System.out.println("11:23 - github.com");
-		
+		System.out.println("ben buradan conflict beklemiyorum");
 		
 	}
 
