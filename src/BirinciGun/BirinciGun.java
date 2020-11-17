32ea06f5835188fad59430a95d8383ae8ef2e0c1
ozgur <74449264+ozgurturan0@users.noreply.github.com>
2020-11-17 23:04:53 +0300

    Update BirinciGun.java

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -9,6 +9,8 @@ public class BirinciGun {
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - Ikinci Gun");
 		System.out.println("10:35 - Ikinci Gun");
+		System.out.println("11:03 - github.com");
+		
 		
 	}
 
