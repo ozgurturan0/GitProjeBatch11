0b78a3aeec0359477833f0af08d56114532b565f
ozgur <ozgurturan0@gmail.com>
2020-11-18 23:06:54 +0300

    master

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -15,6 +15,7 @@ public class BirinciGun {
 		System.out.println("yepyeni bir satir");
 		System.out.println("10:26 - feature branch");
 		System.out.println("10:53 - feature branch");
+		System.out.println("10:53 - master branch");
 		
 	}
 
